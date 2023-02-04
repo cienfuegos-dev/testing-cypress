c0d1ad0ba57e2df9951820c7eaea1876d2635328
cristian-orion <cristian.cienfuegos@lily.ai>
2023-02-04 17:37:46 -0600

    Use cypress cloud

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
+    projectId: "nnk1z4",
     baseUrl: 'http://localhost:3000',
     setupNodeEvents(on, config) {
       // implement node event listeners here
